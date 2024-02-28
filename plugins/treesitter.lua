993c23eed7d7d9350ead06e7715870e6245fd28c
am1fat32 <fat32dev@gmail.com>
2024-02-28 20:40:34 +0400

    Add incremental selection in treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,7 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    }
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
     })
