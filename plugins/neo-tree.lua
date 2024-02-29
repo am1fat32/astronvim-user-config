818ea0a55de5b97c3be3a07e5873ed990a754cab
fat32 <fat32dev@gmail.com>
2024-02-29 17:33:37 +0400

    Refactor neo-tree.lua

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,6 +1,8 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts) opts.enable_normal_mode_for_inputs = true end,
+    opts = {
+      enable_normal_mode_for_inputs = true,
+    },
   },
 }
