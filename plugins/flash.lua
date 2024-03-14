7f1f6b1e1d50f59b453bc816a98855c25ba21a13
fat32 <fat32dev@gmail.com>
2024-03-14 22:47:05 +0400

    Change flash keybindings

--- plugins/flash.lua
+++ plugins/flash.lua
@@ -5,10 +5,10 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "n", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "N", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "M", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<C-n>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<C-m>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
